dbc15a1dd5da48e3ff134a7aac03461197da34d5
80sreplicant <jdnyc84@gmail.com>
2020-02-01 13:27:43 -0500

    all commit

--- app.js
+++ app.js
@@ -1,50 +1,25 @@
 // Set the date you're counting down to
-let count = new Date('September 17, 2020 00:00:00').getTime();
-
-  const x = setInterval(function() {
+let countdown = new Date('September 17, 2020 00:00:00').getTime();
+// update the count down every 1 second
+const x = setInterval(function() {
+  // get todays date and time
   const now = new Date().getTime();
-  const d = count - now;
-
-  const days = Math.floor(d / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((d % (1000 * 60)) / 1000);
-
+  // find the distance between now and the count down date
+  const distance = countdown - now;
+  // Time calculation for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display the result in the element with the id's
   document.querySelector('#days').innerHTML = days;
   document.querySelector('#hours').innerHTML = hours;
   document.querySelector('#minutes').innerHTML = minutes;
   document.querySelector('#seconds').innerHTML = seconds;
-
-  if(d <= 0) {
+  // if the countdown is finished, clear it
+  if (distance <= 0) {
     clearInterval(x);
   }
 }, 1000);
-
-// // Set the date we're counting down to
-// var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-
-// // Update the count down every 1 second
-//   var x = setInterval(function() {
-
-//   // Get today's date and time
-//   var now = new Date().getTime();
-
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   // Display the result in the element with id="demo"
-//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-
-//   // If the count down is finished, write some text
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
